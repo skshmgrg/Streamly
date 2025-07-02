7dd89d018d076cd392392429ee23ed54fd30dedd
skshmgrg <sakshamgarg3050@gmail.com>
2025-07-02 16:32:11 +0530

    Refactor application structure and implement new routes
    - Added multiple new routes for healthcheck, tweets, subscriptions, videos, comments, likes, playlists, and dashboards.
    - Removed user.controller.js and replaced it with user.controllers.js for consistency.
    - Deleted unused user.controller.js and like.model.js files.
    - Created new controller files for comments, dashboards, healthchecks, likes, playlists, subscriptions, tweets, and videos.
    - Updated user.routes.js to reflect the new controller structure.
    - Implemented middleware for JWT verification across all new routes.
    - Enhanced the like model to support likes on videos, comments, and tweets.

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser ,logoutUser, registerUser ,refreshAccessToken, changeCurrentPassword ,getCurrentUser,updateAccountDetails,updateUserAvatar,updateUserCoverImage,getUserChannelProfile,getWatchHistory} from "../controllers/user.controller.js";
+import { loginUser ,logoutUser, registerUser ,refreshAccessToken, changeCurrentPassword ,getCurrentUser,updateAccountDetails,updateUserAvatar,updateUserCoverImage,getUserChannelProfile,getWatchHistory} from "../controllers/user.controllers.js";
 import { upload } from '../middlewares/multer.middleware.js'
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router=Router()
