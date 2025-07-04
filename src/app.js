5d54b62bc263f6b205bcc145e9259bfdd59abd6c
skshmgrg <sakshamgarg3050@gmail.com>
2025-07-05 01:48:31 +0530

    Refactor controller and route imports to use pluralized model names

--- src/app.js
+++ src/app.js
@@ -28,7 +28,7 @@ import dashboardRouter from "./routes/dashboard.routes.js"
 
 //routes declaration
 
-app.use("/api/v1/healthcheck", healthcheckRouter)
+app.use("/api/v1/healthcheck", healthcheckRouter)//tested
 app.use("/api/v1/users", userRouter)
 app.use("/api/v1/tweets", tweetRouter)
 app.use("/api/v1/subscriptions", subscriptionRouter)
--- src/controllers/dashboard.controllers.js
+++ src/controllers/dashboard.controllers.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
-import { Video } from "../models/video.model.js";
+import { Video } from "../models/video.models.js";
 import { User } from "../models/user.models.js";
-import { Subscription } from "../models/subscription.model.js";
-import { Like } from "../models/like.model.js";
+import { Subscription } from "../models/subscription.models.js";
+import { Like } from "../models/like.models.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
